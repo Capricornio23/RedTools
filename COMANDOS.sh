797465ead9d29bf161d58c85f631dd1f5f830e20
Capricornio23 <80604878+Capricornio23@users.noreply.github.com>
2021-03-13 18:27:04 -0500

    Add files via upload

---
+++ COMANDOS.sh
@@ -0,0 +1,615 @@
+#!/bin/bash
+# Create by Benyamin-creator
+# COMANDOS
+
+1) DayFish
+
+# DayFish
+
+# 🔰INSTALACIÓN🔰:
+
+# Clonamos el repositorio con:
+
+$ git clone https://github.com/Benyamin-creator/DayFish
+
+# accedemos a la herramienta con:
+
+$ cd DayFish
+
+# Damos permiso de ejecución con:
+
+$ chmod +x DayFish
+
+# Ejecutamos con:
+
+$ bash DayFish.sh
+
+# 🔰PROGRAMADOR🔰
+
+ BENYAMIN-CREATOR
+ CANAL DE YOUTUBE: BENYAMIN2000
+
+2) DayFish1.1
+
+# DayFish1.1
+
+# 🔰INSTALACIÓN🔰:
+
+# Clonamos el repositorio con:
+
+$ git clone https://github.com/Benyamin-creator/DayFish1.1
+
+# accedemos a la herramienta con:
+
+$ cd DayFish1.1
+
+# Damos permiso de ejecución con:
+
+$ chmod +x DayFish1.1
+
+# Ejecutamos con:
+
+$ bash DayFish
+
+# 🔰PROGRAMADOR🔰
+
+ BENYAMIN-CREATOR
+ CANAL DE YOUTUBE: BENYAMIN2000
+
+3) RedFish
+
+# RedFish
+
+# 🔰INSTALACIÓN🔰:
+
+# Clonamos el repositorio con:
+
+$ git clone https://github.com/Benyamin-creator/RedFish
+
+# accedemos a la herramienta con:
+
+$ cd RedFish
+
+# Damos permiso de ejecución con:
+
+$ chmod +x RedFish.sh
+
+# Ejecutamos con:
+
+$ bash RedFish.sh
+
+# 🔰PROGRAMADOR🔰
+
+ BENYAMIN-CREATOR
+ CANAL DE YOUTUBE: BENYAMIN2000
+
+4) FRI3NDS
+
+# FRI3NDS
+"Es una herramienta que te permite hackear cámaras vía webcam"
+
+# 🔰INSTALACIÓN🔰
+
+$ pkg update
+
+$ pkg upgrade
+
+$ pkg install git
+
+$ pkg install nano
+
+$ pkg install python
+
+$ git clone https://github.com/Benyamin-creator/FRI3NDS
+
+$ cd FRI3NDS
+
+$ chmod 777 friends
+
+$ python2 friends.py
+
+# 🔰PROGRAMADOR🔰
+
+$ BENYAMIN-CREATOR
+
+$ Canal de YouTube: Benyamin2000
+
+$ Gracias por su apoyo cracks XD
+
+5) FACTORY
+
+# FACTORY
+
+Es una herramienta que te permite crear virus
+en apk
+
+# Puedes instalarla siguiendo estos pasos:
+
+$ git clone https://github.com/Benyamin-creator/FACTORY
+
+$ cd FACTORY
+
+$ chmod +x *
+
+$ bash factory.sh
+
+Elige una opción, el virus se guardará
+en la memoria interna
+
+# 🔰PROGRAMADOR🔰
+
+$ BENYAMIN-CREATOR
+
+$ Canal de YouTube: Benyamin2000
+
+6) HACK-CAM
+
+# 🔰📷HACK-CAM📷🔰
+
+"Es un script que sirve para hackear la cámara frontal de un
+dispositivo, por intermedio de un Link y así tomar imágenes
+de dicho dispositivo hackeado."
+
+
+# 🔰PLATAFORMAS🔰
+
+[√] Termux
+[√] LINUX
+[√] Parrot
+
+# 🔰REQUISITOS🔰
+
+pkg install git -y
+
+🔰INSTALACIÓN🔰
+
+git clone https://github.com/Benyamin-creator/HACK-CAM
+
+cd HACK-CAM
+
+chmod 777 hack-cam.sh
+
+bash hack-cam.sh
+
+# 🔰PROGRAMADOR🔰
+
+Creado por BENYAMIN-CREATOR
+
+WHITE HACKS ©2020
+
+7) F-BRUTE
+
+# F-BRUTE
+
+It is a tool to crack Facebook
+passwords using brute force
+
+# We clone the github repository with:
+
+$ git clone https://github.com/Benyamin-creator/F-BRUTE
+
+# We enter the directory with:
+
+$ cd F-BRUTE
+
+# We give permissions with:
+
+$ chmod +x *
+
+# We execute with:
+
+$ python2 brute.py
+
+# CODED By: Benyamin-creator (RED-SOCIAL)
+
+8) IP-E
+
+#🔰🔎🌎IP-E🌎🔍🔰
+
+Es una herramienta que te genera datos de una IP determinada
+
+#🔰PLATAFORMAS🔰
+
+[√] Termux
+[√] LINUX
+[√] Parrot
+
+#🔰REQUISITOS🔰
+
+pkg install git -y
+
+#🔰INSTALACIÓN🔰
+
+git clone https://github.com/Benyamin-creator/IP-E
+
+cd IP-E
+
+chmod 777 IP-ESPIA.py
+
+python IP-ESPIA.py
+
+#🔰PROGRAMADOR🔰
+
+Creado por BENYAMIN-CREATOR
+
+WHITE HACKS ©2020
+
+9) D-FB1
+
+# D-FB1
+
+It is a tool that allows you to hack passwords
+by brute force, it can be
+applied to four types of social networks such as:
+Twitter, GMAIL, Facebook and Instagram
+
+# how to install
+
+# We clone the github repository with:
+
+$ git clone https://github.com/Benyamin-creator/D-FB1
+
+#  We enter the directory with:
+
+$ cd D-FB1
+
+# We give permissions with:
+
+$ chmod +x *
+
+# We execute with:
+
+# First, create your dictionary with any of the following options:
+
+#
+
+$ python WORDLISTER.py
+
+$ python Dragon.py
+
+# now run the tool with:
+
+$ bash Cobra-Team.sh
+
+# CODED By: Benyamin-creator(RED-SOCIAL)
+
+10) A-DOS
+
+# A-DOS
+Tool that is used for Dos attacks
+on web pages,
+but it is done with
+the execution of said tool
+by several people (10 to 15 people)
+
+# We clone the github repository with:
+
+$  git clone https://github.com/Benyamin-creator/A-DOS
+
+# We enter the directory with:
+
+$ cd A-Dos
+
+# We give permissions with:
+
+$ chmod + x *
+
+# We execute with:
+
+# You can choose between two options and it can be done as follows:
+
+$ python2 D-dos.py
+
+$ python H-Dos.py
+
+# CODED By: Benyamin-creator (RED-SOCIAL)
+
+11) BF-GMAIL
+
+# BF-GMAIL
+
+BF-GMAIL es una herramienta que
+sirve para crackear contraseñas de gmail
+mediante fuerza bruta utilizando hydra
+
+# how to install
+
+$ git clone https://github.com/Benyamin-creator/BF-GMAIL
+
+$ cd BF-GMAIL
+
+$ chmod +x GMAIL-BRUTE-FORCE.sh
+
+$ bash GMAIL-BRUTE-FORCE.sh
+
+# CODED By: Benyamin-RED-SOCIAL
+
+12) FB-INFORMATION
+
+# FB-INFORMATION
+It is a tool that allows you to
+obtain information from a
+Facebook profile
+
+# We clone the github repository with:
+
+$ git clone https://github.com/Benyamin-creator/FB-INFORMATION
+
+# We enter the directory with:
+
+$ cd FB-INFORMATION
+
+# We give permissions with:
+
+$ chmod + x * # We execute with:
+
+$ python2 FB-INFORMATION.py
+
+# CODED By: Benyamin-creator (RED-SOCIAL)
+
+13) FotoSploit
+
+FotoSploit
+FotoSploit es un exploit que ensambla la foto
+de nuestra víctima en un enlace falso {phishing}
+
+FotoSploit in
+Kali linux [nethunter]
+UserLand
+ubuntu
+parrot
+etc..
+Usage: https://youtu.be/sH15XKPVToA
+Banner FotoSploit
+Screenshot_20191202-134419_Termux~01
+
+# Installation
+
+$ pkg update && pkg upgrade -y
+$ pkg install -y php
+$ pkg install -y python2
+$ pkg install -y git
+$ cd $HOME
+$ git clone https://github.com/Cesar-Hack-Gray/FotoSploit
+$ cd FotoSploit
+$ ls
+$ bash install.sh
+$ ./FotoSploit
+
+# Installation FotoSploit premium
+
+$ pkg update && pkg upgrade -y
+$ pkg install -y php
+$ pkg install -y python2
+$ pkg install -y git
+$ CD $HOME
+$ git clone https://github.com/Cesar-Hack-Gray/FotoSploit
+$ cd FotoSploit
+$ ls
+$ bash install.sh --install --premium
+$ ./FotoSploit [option]
+
+14) SocialSploit
+
+# SocialSploit
+
+# Installation
+$ pkg install -y git
+$ git clone https://github.com/Cesar-Hack-Gray/SocialSploit
+$ cd SocialSploit
+$ ls
+$ bash install.sh
+$ ./Sploit
+
+# Installation
+https://youtu.be/z9FmwGOo3A8
+Example
+CollageMaker_20191115_193409432
+
+● SocialSploit es un framework de phishing que nos ayuda a hackear
+con ngrok y serveo :3
+● se trata de ingeniería social recuerda esto no me hago responsable
+del mal uso.
+
+# requisitos ->  curl, php, ssh, python2 y wget
+
+15) infectador-framework
+
+# infectador-framework
+
+$ pkg install php
+$ pkg install git
+$ git clone https://github.com/Cesar-Hack-Gray/infectador-framework
+$ cd infectador-framework
+$ chmod +x Infectador
+$ bash Infectador
+
+16) Spayer
+
+Spayer
+Spayer multi explotadores >> Spayer Framework >> by CesarHackGray
+    Spayer - Framework
+    Spayer es una plataforma de hacking
+    Que nos permite hackear y usar cada una
+    de mas herramientas que contiene spayer
+Example
+Screenshot_20190628-083358~01
+
+Example 2
+2do menu de Spayer framework
+Screenshot_20190628-083050~01
+
+informacion de las herramientas de Spayer
+   Nos permite hacer spam a correos electronicos
+   y nos permite crear payloads para linux
+   payloads para android y backdoor a apks
+   y tenemos la herramienta adentro de extraer
+   los databases de una fotografia (exit-tool)
+   y tenemos la herramienta phishing adentro tambien
+   y la herramienta hack wifi metodo phishing
+   hay un poco de todo en Spayer
+Screenshot_20190628-083152~01
+
+Installation
+   git clone https://github.com/Cesar-Hack-Gray/Spayer
+   cd Spayer
+   ls
+   bash install.sh
+   bash Spayer
+Requisitos
+   metasploit-framework
+   apktool
+   php
+   java
+
+17) scam
+
+# Scam
+
+$ apt update && apt upgrade -y
+$ apt install git
+$ git clone https://github.com/Cesar-Hack-Gray/scam
+$ cd scam
+$ chmod +x *
+$ bash install.sh
+$ ./phishing.sh
+
+18) NgrokTH
+
+📱¿Qué es Ngrok?📱
+
+Ngrok es una herramienta que permite acceder nuestro servidor
+local a cualquier persona en internet con la que compartamos
+una url generada dinamicamente.
+
+📱INSTALACIÓN DE Ngrok EN TERMUX (ANDROID)📱
+
+$ apt update && apt upgrade -y
+
+$ termux-setup-storage
+
+$ pkg install -y git
+
+$ git clone https://github.com/TermuxHacking000/NgrokTH
+
+$ cd NgrokTH
+
+$ chmod 711 ngrok.sh
+
+$ ./ngrok.sh
+
+Escribe el authtoken de tu cuenta Ngrok y pulsa Enter.
+
+19) DICTIONARY-GENERATOR
+
+# DICTIONARY-GENERATOR
+
+Tool that allows you to generate
+dictionaries for brute force attacks,
+the file is found with a ".txt" extension
+
+The important thing is that dictionaries
+can be used to attack different types 
+of social networks, etc.
+
+# how to install
+
+# We clone the github repository with:
+
+$ git clone https://github.com/Benyamin-creator/DICTIONARY-GENERATOR
+
+# We enter the directory with:
+
+$ cd DICTIONARY-GENERATOR
+
+# We give permissions with:
+
+$ chmod +x *
+
+# We execute with:
+
+$ python2 DICTIONARY-GENERATOR.py
+
+# CODED By: Benyamin-creator(RED-SOCIAL)
+
+20) Email-Supplant
+
+# Email-Supplant
+Email-Suppalt It is a tool to hack Email passwords using brute force
+
+# how to install
+
+# We clone the github repository with:
+
+$ git clone https://github.com/Benyamin-creator/Email-Suppalt
+
+# We enter the directory with:
+
+$ cd Email-Suppalt
+
+# We give permissions with:
+
+$ chmod + x *
+
+# We execute with:
+
+$ bash Email-Suppalt.sh
+
+# CODED By: Benyamin-creator (RED-SOCIAL)
+
+21) VIRUS-CREATE
+
+# VIRUS-CREATE
+
+Apk virus creation tool
+
+# We clone the github repository with:
+
+$ git clone https://github.com/Benyamin-creator/VIRUS-CREATE
+
+# We enter the directory with:
+
+$ cd VIRUS-CREATE
+
+# We give permissions with:
+
+$ chmod +x *
+
+# We execute with:
+
+$ python2 VIRUS-CREATE.py
+
+# CODED By: Benyamin-creator (RED-SOCIAL)
+
+30) TOOLS-KILLER-ON-THE-NET
+
+# TOOLS-KILLER-ON-THE-NET
+
+It is a tool that allows you to
+install different HACKING
+and PENTESTING tools
+
+# We clone the github repository with:
+
+$ git clone https://github.com/Benyamin-creator/TOOLS-KILLER-ON-THE-NET
+
+# We enter the directory with:
+
+$ cd TOOLS-KILLER-ON-THE-NET
+
+# We give permissions with:
+
+$ chmod + x *
+
+# We execute with:
+
+$ bash tipos.sh
+
+# CODED By: Benyamin-creator(RED-SOCIAL)
+
+
+echo "ESO ES TODO CRACK"
+
+"GRACIAS POR USAR MI SCRIPT"
+
+Att: "BENYAMIN-CREATOR"
